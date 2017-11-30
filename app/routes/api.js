c17da267dfd98b016d076f654b1b3e52c072b266
Abhinav Gupta <abhinav3006gupta@gmail.com>
2017-11-30 14:02:19 +0530

    initial

---
+++ app/routes/api.js
@@ -0,0 +1,6 @@
+/**
+ * Created by abhi on 30-Nov-17.
+ */
+var jwt = require('jsonwebtoken'); // Import JWT Package
+var secret = 'harrythepotter'; // Create custom secret for use in JWT
+var nodemailer = require('nodemailer'); // Import Nodemailer Package
