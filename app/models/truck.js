1ee0572ae518afa3d458c7050bc98518e3e72397
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2017-12-01 13:29:54 +0530

    changed bcrypt to bcrypt-nodejs

--- app/models/truck.js
+++ app/models/truck.js
@@ -3,7 +3,7 @@
  */
 var mongoose = require('mongoose'); // Import Mongoose Package
 var Schema   = mongoose.Schema; // Assign Mongoose Schema function to variable
-var bcrypt   = require('bcrypt'); // Import Bcrypt Package
+var bcrypt   = require('bcrypt-nodejs'); // Import Bcrypt Package
 var titlize  = require('mongoose-title-case'); // Import Mongoose Title Case Plugin
 var validate = require('mongoose-validator'); // Import Mongoose Validator Plugin
 
